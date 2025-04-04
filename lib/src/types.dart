6da6d8d8e0874ac870e2976bebe72c3e5efc30cb
Roman Brazhnyk <roman.b@affinidi.com>
2025-04-04 21:49:03 +0800

    feat: consolidate algo names

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -3,11 +3,10 @@ enum KeyType { secp256k1, ed25519, x25519, rsa }
 enum HashingAlgorithm { sha256, sha512 }
 
 enum SignatureScheme {
-  es256k("ES256K", "EcdsaSecp256k1Signature2019", KeyType.secp256k1,
+  es256k("ES256K", "ecdsa-secp256r1-sha256", KeyType.secp256k1,
       HashingAlgorithm.sha256),
-  eddsa("EdDSA", null, KeyType.ed25519, HashingAlgorithm.sha512),
-  ed25519sha256(
-      null, "Ed25519Signature2020", KeyType.ed25519, HashingAlgorithm.sha256),
+  eddsa("EdDSA", "ed25519", KeyType.ed25519, HashingAlgorithm.sha512),
+  ed25519sha256("EdDSA", "ed25519-sha256", KeyType.ed25519, HashingAlgorithm.sha256),
   rsa("RS256", "rsa-pkcs1-sha256", KeyType.rsa, HashingAlgorithm.sha256);
 
   final String? jwtName;
--- test/fixtures/verifiable_credentials_data_fixtures.dart
+++ test/fixtures/verifiable_credentials_data_fixtures.dart
@@ -18,7 +18,7 @@ class VerifiableCredentialDataFixtures {
         'expirationDate': '2024-07-18T20:16:05.648',
         'issuer': 'did:key:aaaabaaaabaaaabaaaabaaaabaaaabaaaabaaaabaaaabaaaa',
         'proof': {
-          'type': 'EcdsaSecp256k1Signature2019',
+          'type': 'ecdsa-secp256r1-sha256',
           'created': '2024-07-16T18:16:05Z',
           'proofPurpose': 'assertionMethod',
           'verificationMethod':
