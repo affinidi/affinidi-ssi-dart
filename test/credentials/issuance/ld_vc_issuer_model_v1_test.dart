de6d9a2e3b10b71e54bbca4cc00716e683ac22c0
Cosmin Mogos <cosmin.m@affinidi.com>
2025-04-08 11:25:01 +0200

    fix: linter

--- test/credentials/issuance/ld_vc_issuer_model_v1_test.dart
+++ test/credentials/issuance/ld_vc_issuer_model_v1_test.dart
@@ -4,7 +4,6 @@ import 'dart:typed_data';
 import 'package:base_codecs/base_codecs.dart';
 import 'package:ssi/src/credentials/issuance/ecdsa_secp256k1_signature2019_suite.dart';
 import 'package:ssi/src/credentials/models/v1/vc_data_model_v1.dart';
-import 'package:ssi/src/did/did_signer.dart';
 import 'package:ssi/ssi.dart';
 import 'package:test/test.dart';
 
