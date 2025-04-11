87f392ec7474d1e5545108b3976f6e4deb803c5d
Cosmin Mogos <cosmin.m@affinidi.com>
2025-04-11 21:02:51 +0200

    remove unnecessary constructors

--- lib/src/credentials/presentations/linked_data/ld_vp_data_model_v1.dart
+++ lib/src/credentials/presentations/linked_data/ld_vp_data_model_v1.dart
@@ -5,21 +5,7 @@ import '../models/v1/vp_data_model_v1.dart';
 
 class LdVpDataModelV1 extends VpDataModelV1
     implements ParsedVerifiablePresentation<String> {
-  String? _serialized;
-
-  LdVpDataModelV1({
-    required super.context,
-    required super.id,
-    required super.holder,
-    required super.type,
-    super.proof,
-    super.verifiableCredential,
-    required String serialized,
-  }) : _serialized = serialized;
-
-  LdVpDataModelV1.fromJson(super.input)
-      : // use parsing from VcDataModelV1
-        super.fromJson();
+  final String _serialized;
 
   LdVpDataModelV1.fromParsed(String serialized, super.input)
       : _serialized = serialized,
@@ -28,7 +14,6 @@ class LdVpDataModelV1 extends VpDataModelV1
 
   @override
   String get serialized {
-    _serialized ??= jsonEncode(toJson());
-    return _serialized!;
+    return _serialized;
   }
 }
