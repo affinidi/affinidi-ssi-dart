da302890261bdc9754348f5423e9009ab7ede3af
Olena Iskryzhytska <olena.i@affinidi.com>
2025-04-15 10:00:07 +0300

    chore: fix linting

--- lib/src/did/did_document.dart
+++ lib/src/did/did_document.dart
@@ -66,7 +66,7 @@ class DidDocument implements JsonObject {
     } else {
       throw FormatException('id property needed in did document');
     }
-    
+
     if (document.containsKey('alsoKnownAs')) {
       alsoKnownAs = document['alsoKnownAs'].cast<String>();
     }
@@ -222,7 +222,8 @@ class DidDocument implements JsonObject {
         if (veriMap.containsKey(entry)) newList.add(veriMap[entry]);
       } else {
         throw SsiException(
-          message: 'Element $entry has unsupported Datatype ${entry.runtimeType}',
+          message:
+              'Element $entry has unsupported Datatype ${entry.runtimeType}',
           code: SsiExceptionType.invalidDidDocument.code,
         );
       }
@@ -568,7 +569,8 @@ class ServiceEndpoint implements JsonObject {
   late dynamic serviceEndpoint;
   late dynamic accept;
 
-  ServiceEndpoint({required this.id, required this.type, required this.serviceEndpoint});
+  ServiceEndpoint(
+      {required this.id, required this.type, required this.serviceEndpoint});
 
   ServiceEndpoint.fromJson(dynamic jsonObject) {
     final se = jsonToMap(jsonObject);
@@ -585,7 +587,8 @@ class ServiceEndpoint implements JsonObject {
     if (se.containsKey('serviceEndpoint')) {
       serviceEndpoint = se['serviceEndpoint'];
     } else {
-      throw FormatException('serviceEndpoint property is needed in serviceEndpoint');
+      throw FormatException(
+          'serviceEndpoint property is needed in serviceEndpoint');
     }
     if (se.containsKey('accept')) {
       accept = se['accept'];
