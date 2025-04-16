58352b35c4e7d83df9837482bb107e007f5ca2cf
Olena Iskryzhytska <olena.i@affinidi.com>
2025-04-16 16:36:03 +0300

    chore: address comments

--- test/did/did_resolver_test.dart
+++ test/did/did_resolver_test.dart
@@ -1,13 +1,8 @@
 import 'dart:convert';
 
-import 'package:base_codecs/base_codecs.dart';
-import 'package:ssi/src/did/did_key.dart';
-import 'package:ssi/src/did/did_peer.dart';
 import 'package:ssi/src/did/did_resolver.dart';
 import 'package:ssi/src/exceptions/ssi_exception.dart';
 import 'package:ssi/src/exceptions/ssi_exception_type.dart';
-import 'package:ssi/src/wallet/bip32_ed25519_wallet.dart';
-import 'package:ssi/src/wallet/bip32_wallet.dart';
 import 'package:test/test.dart';
 
 import '../fixtures/did_document_fixtures.dart';
@@ -16,36 +11,25 @@ void main() {
   group("When resolving did document", () {
     group("using did:key,", () {
       test("it resolves succesfully", () async {
-        final seed = hexDecode(
-          'a1772b144344781f2a55fc4d5e49f3767bb0967205ad08454a09c76d96fd2ccd',
-        );
-        final wallet = Bip32Wallet.fromSeed(seed);
-        final rootKeyId = "0-0";
-        final keyPair = await wallet.getKeyPair(rootKeyId);
-        final doc = await DidKey.create(keyPair);
+        final did = "did:key:zQ3shd83o9cAdtd5SFF8epKAqDBpMV3x9f3sbv4mMPV8uaDC2";
 
         final expectedDidDoc =
             jsonDecode(DidDocumentFixtures.didDocumentWithControllerKey);
 
-        final resolvedDidDocument = await resolveDidDocument(doc.id);
+        final resolvedDidDocument = await resolveDidDocument(did);
         expect(resolvedDidDocument.toJson(), expectedDidDoc);
       });
     });
 
     group("using did:peer,", () {
       test("it resolves successfully", () async {
-        final seed = hexDecode(
-          'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542',
-        );
-        final wallet = await Bip32Ed25519Wallet.fromSeed(seed);
-        final rootKeyId = "0-0";
-        final keyPair = await wallet.getKeyPair(rootKeyId);
-        final doc = await DidPeer.create([keyPair]);
+        final did =
+            "did:peer:0z6Mkp92myXtWkQYxhFmDxqkTwURYZAEjUm9iAuZxyjYzmfSy";
 
         final expectedDidDoc =
             jsonDecode(DidDocumentFixtures.didDocumentWithControllerPeer);
 
-        final resolvedDidDoc = await resolveDidDocument(doc.id);
+        final resolvedDidDoc = await resolveDidDocument(did);
 
         expect(resolvedDidDoc.toJson(), expectedDidDoc);
       });
