13a4cbc1d1d9120614d5fa5580ee74ce14f5ecfe
Vinuth Madinur <vinuth.m@affinidi.com>
2025-04-25 12:20:27 +0530

    fix: renaming for consistency

---
+++ lib/src/credentials/models/v1/mutable_vc_data_model_v1.dart
@@ -0,0 +1,85 @@
+import 'package:ssi/src/credentials/models/field_types/evidence.dart';
+import 'package:ssi/src/credentials/models/field_types/refresh_service/v1.dart';
+import 'package:ssi/src/credentials/models/field_types/terms_of_use.dart';
+
+import '../../proof/embedded_proof.dart';
+import '../field_types/credential_schema.dart';
+import '../field_types/credential_status/v1.dart';
+import '../field_types/credential_subject.dart';
+import '../field_types/holder.dart';
+import '../field_types/issuer.dart';
+import 'vc_data_model_v1.dart';
+
+class MutableVcDataModelV1 extends VcDataModelV1 {
+  @override
+  List<String> context;
+
+  @override
+  Uri? id;
+
+  @override
+  List<String> type;
+
+  @override
+  List<MutableCredentialSchema> credentialSchema;
+
+  @override
+  List<MutableCredentialSubject> credentialSubject;
+
+  @override
+  MutableIssuer? issuer;
+
+  @override
+  DateTime? issuanceDate;
+
+  @override
+  DateTime? expirationDate;
+
+  @override
+  DateTime? get validFrom => issuanceDate;
+
+  @override
+  DateTime? get validUntil => expirationDate;
+
+  @override
+  MutableHolder? holder;
+
+  @override
+  List<EmbeddedProof> proof;
+
+  @override
+  MutableCredentialStatusV1? credentialStatus;
+
+  @override
+  List<MutableRefreshServiceV1> refreshService;
+
+  @override
+  List<MutableTermsOfUse> termsOfUse;
+
+  @override
+  List<MutableEvidence> evidence;
+
+  MutableVcDataModelV1({
+    List<String>? context,
+    this.id,
+    List<MutableCredentialSchema>? credentialSchema,
+    List<MutableCredentialSubject>? credentialSubject,
+    this.issuer,
+    List<String>? type,
+    this.issuanceDate,
+    this.expirationDate,
+    this.holder,
+    List<EmbeddedProof>? proof,
+    this.credentialStatus,
+    List<MutableRefreshServiceV1>? refreshService,
+    List<MutableTermsOfUse>? termsOfUse,
+    List<MutableEvidence>? evidence,
+  })  : context = context ?? [],
+        credentialSchema = credentialSchema ?? [],
+        credentialSubject = credentialSubject ?? [],
+        type = type ?? [],
+        proof = proof ?? [],
+        refreshService = refreshService ?? [],
+        termsOfUse = termsOfUse ?? [],
+        evidence = evidence ?? [];
+}
