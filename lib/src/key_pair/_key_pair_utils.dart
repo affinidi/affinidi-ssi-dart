5238e42ba5d32a76c80f699b42f57cea6302df85
Denys Popov <denys.p@affinidi.com>
2025-07-08 14:43:17 +0300

    fix: make formatter happy

--- lib/src/key_pair/_key_pair_utils.dart
+++ lib/src/key_pair/_key_pair_utils.dart
@@ -1,12 +1,4 @@
-import 'dart:typed_data';
 import './_encryption_utils.dart';
 
 /// The encryption utility instance.
 final encryptionUtils = EncryptionUtils();
-
-/// Converts a [BigInt] to bytes.
-Uint8List _bigIntToBytes(BigInt value) {
-  return value.isNegative
-      ? encryptionUtils.intToBytes(value)
-      : encryptionUtils.unsignedIntToBytes(value);
-}
