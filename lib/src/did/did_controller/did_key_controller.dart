2ee0f28df11d31f2c6039a91b1e0a5820afb65a7
Soroush Yousefpour <soroush.y@affinidi.com>
2025-07-09 10:26:42 +0200

    fix: update pubspec versioning (#140)
    * fix: remove primaryPublicKey
    
    * fix: update version to 2.0.0 in pubspec.yaml
    
    * refactor: rename primaryPublicKey to didSourceKey for clarity in DidController methods
    
    * feat: add factory method for creating and initializing DidController instances
    
    * refactor: update comment for removing verification method in DidController
    
    * chore: remove reference submodules from the repository
    
    * refactor: complete removal of didSourceKey parameter from DID controllers
    
    * fix: update example in documentation to use DidPeerController for initialization
    
    * fix: update documentation examples to reflect correct usage of DidController
    
    ---------
    
    Co-authored-by: Cosmin Mogos <cosmin.m@affinidi.com>

--- lib/src/did/did_controller/did_key_controller.dart
+++ lib/src/did/did_controller/did_key_controller.dart
@@ -74,11 +74,9 @@ class DidKeyController extends DidController {
   }
 
   @override
-  Future<String> buildVerificationMethodId(PublicKey publicKey,
-      {PublicKey? primaryPublicKey}) async {
+  Future<String> buildVerificationMethodId(PublicKey publicKey) async {
     // For did:key, the DID itself is derived from the public key.
-    final didKey = primaryPublicKey ?? publicKey;
-    final didMultikey = toMultikey(didKey.bytes, didKey.type);
+    final didMultikey = toMultikey(publicKey.bytes, publicKey.type);
     final didMultibase = toMultiBase(didMultikey);
     final did = 'did:key:$didMultibase';
 
