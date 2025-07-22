dfc2642a4436b18062e67a060b6191eb2de65606
Kostiantyn Synyshyn <kostiantyn.s@affinidi.com>
2025-07-23 00:32:27 +0300

    fix: update test to include cross check

--- test/did/did_manager/did_manager_store_test.dart
+++ test/did/did_manager/did_manager_store_test.dart
@@ -4,11 +4,9 @@ import 'package:test/test.dart';
 void main() {
   group('InMemoryDidStore', () {
     late InMemoryDidKeyMappingStore keyMappingStore;
-    late InMemoryDidDocumentReferenceStore documentReferenceStore;
 
     setUp(() {
       keyMappingStore = InMemoryDidKeyMappingStore();
-      documentReferenceStore = InMemoryDidDocumentReferenceStore();
     });
 
     group('Map/get/remove operations', () {
@@ -258,7 +256,7 @@ void main() {
     group('Custom DidStore implementation example', () {
       test('should work with custom implementation', () async {
         // Example of a custom implementation that could be used
-        final customStore = _CustomPersistentStore();
+        final customStore = InMemoryDidKeyMappingStore();
 
         // Should implement the same interface
         await customStore.setMapping('did:key:test#key', 'wallet-test');
@@ -277,147 +275,4 @@ void main() {
       });
     });
   });
-}
-
-// Example custom implementation for testing
-class _CustomPersistentStore extends DidStore {
-  final Map<String, String> _storage = {};
-  final List<String> _authentication = [];
-  final List<String> _keyAgreement = [];
-  final List<String> _capabilityInvocation = [];
-  final List<String> _capabilityDelegation = [];
-  final List<String> _assertionMethod = [];
-  final List<ServiceEndpoint> _serviceEndpoints = [];
-
-  @override
-  Future<void> setMapping(
-      String verificationMethodId, String walletKeyId) async {
-    _storage[verificationMethodId] = walletKeyId;
-    // In a real implementation, this might persist to disk/database
-  }
-
-  @override
-  Future<String?> getWalletKeyId(String verificationMethodId) async {
-    return _storage[verificationMethodId];
-  }
-
-  @override
-  Future<void> removeMapping(String verificationMethodId) async {
-    _storage.remove(verificationMethodId);
-  }
-
-  @override
-  Future<void> clearAll() async {
-    _storage.clear();
-    await clearVerificationMethodReferences();
-    await clearServiceEndpoints();
-  }
-
-  @override
-  Future<List<String>> get verificationMethodIds async =>
-      _storage.keys.toList();
-
-  @override
-  Future<List<String>> get authentication async => _authentication;
-
-  @override
-  Future<List<String>> get keyAgreement async => _keyAgreement;
-
-  @override
-  Future<List<String>> get capabilityInvocation async => _capabilityInvocation;
-
-  @override
-  Future<List<String>> get capabilityDelegation async => _capabilityDelegation;
-
-  @override
-  Future<List<String>> get assertionMethod async => _assertionMethod;
-
-  @override
-  Future<List<ServiceEndpoint>> get serviceEndpoints async => _serviceEndpoints;
-
-  @override
-  Future<void> addAuthentication(String verificationMethodId) async {
-    if (!_authentication.contains(verificationMethodId)) {
-      _authentication.add(verificationMethodId);
-    }
-  }
-
-  @override
-  Future<void> removeAuthentication(String verificationMethodId) async {
-    _authentication.remove(verificationMethodId);
-  }
-
-  @override
-  Future<void> addKeyAgreement(String verificationMethodId) async {
-    if (!_keyAgreement.contains(verificationMethodId)) {
-      _keyAgreement.add(verificationMethodId);
-    }
-  }
-
-  @override
-  Future<void> removeKeyAgreement(String verificationMethodId) async {
-    _keyAgreement.remove(verificationMethodId);
-  }
-
-  @override
-  Future<void> addCapabilityInvocation(String verificationMethodId) async {
-    if (!_capabilityInvocation.contains(verificationMethodId)) {
-      _capabilityInvocation.add(verificationMethodId);
-    }
-  }
-
-  @override
-  Future<void> removeCapabilityInvocation(String verificationMethodId) async {
-    _capabilityInvocation.remove(verificationMethodId);
-  }
-
-  @override
-  Future<void> addCapabilityDelegation(String verificationMethodId) async {
-    if (!_capabilityDelegation.contains(verificationMethodId)) {
-      _capabilityDelegation.add(verificationMethodId);
-    }
-  }
-
-  @override
-  Future<void> removeCapabilityDelegation(String verificationMethodId) async {
-    _capabilityDelegation.remove(verificationMethodId);
-  }
-
-  @override
-  Future<void> addAssertionMethod(String verificationMethodId) async {
-    if (!_assertionMethod.contains(verificationMethodId)) {
-      _assertionMethod.add(verificationMethodId);
-    }
-  }
-
-  @override
-  Future<void> removeAssertionMethod(String verificationMethodId) async {
-    _assertionMethod.remove(verificationMethodId);
-  }
-
-  @override
-  Future<void> addServiceEndpoint(ServiceEndpoint endpoint) async {
-    if (!_serviceEndpoints.any((se) => se.id == endpoint.id)) {
-      _serviceEndpoints.add(endpoint);
-    }
-  }
-
-  @override
-  Future<void> removeServiceEndpoint(String id) async {
-    _serviceEndpoints.removeWhere((se) => se.id == id);
-  }
-
-  @override
-  Future<void> clearVerificationMethodReferences() async {
-    _authentication.clear();
-    _keyAgreement.clear();
-    _capabilityInvocation.clear();
-    _capabilityDelegation.clear();
-    _assertionMethod.clear();
-  }
-
-  @override
-  Future<void> clearServiceEndpoints() async {
-    _serviceEndpoints.clear();
-  }
 }
