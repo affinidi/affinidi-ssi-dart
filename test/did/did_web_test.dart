d7dfdd3605c5341d216be8509a9cb8e3ed3617db
Mohamed Ibrahim <mohamed.i@affinidi.com>
2025-07-23 16:08:47 +0200

    refactor: remove live key

--- test/did/did_web_test.dart
+++ test/did/did_web_test.dart
@@ -14,21 +14,6 @@ void main() {
       expect(uri.toString(), 'https://example.com/user/did.json');
     });
 
-    test('resolves did:web successfully', () async {
-      final did = 'did:web:demo.spruceid.com'; // JWK-based
-      // final did = 'did:web:identity.foundation'; // Base58-based
-      // final did = ? // publicKeyMultibase-based
-      final doc = await DidWeb.resolve(did);
-
-      expect(doc.id, equals(did));
-      expect(doc.toJson(), contains('id'));
-      expect(doc.toJson(), contains('verificationMethod'));
-      expect(
-        doc.context.hasUrlContext(Uri.parse('https://www.w3.org/ns/did/v1')),
-        true,
-      );
-    });
-
     test('throws SsiException on non-200 response', () async {
       final did = 'did:key:zQ3shd83o9cAdtd5SFF8epKAqDBpMV3x9f3sbv4mMPV8uaDC2';
 
