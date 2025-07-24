2f3e8cba7086395be91d026bcdb4bea41425404f
Soroush Yousefpour <soroush.y@affinidi.com>
2025-07-24 16:39:47 +0200

    fix: change Jwk doc type from Map<String, dynamic> to Map<String, String>

--- lib/src/did/did_document/did_document.dart
+++ lib/src/did/did_document/did_document.dart
@@ -327,7 +327,7 @@ class DidDocument implements JsonObject {
 /// Represents a JSON Web Key (JWK) used in DID documents.
 class Jwk {
   /// The JWK document as a map.
-  late final Map<String, dynamic> doc;
+  late final Map<String, String> doc;
 
   /// Creates a [Jwk] from JSON data.
   ///
@@ -336,7 +336,7 @@ class Jwk {
     final map = jsonToMap(input);
 
     try {
-      doc = Map<String, dynamic>.from(map);
+      doc = Map<String, String>.from(map);
     } catch (error, stackTrace) {
       Error.throwWithStackTrace(
         SsiException(
@@ -350,7 +350,7 @@ class Jwk {
   }
 
   /// Converts the JWK to JSON.
-  Map<String, dynamic> toJson() {
+  Map<String, String> toJson() {
     return doc;
   }
 }
