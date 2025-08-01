bc9e305c88ddda1918188d97204c082f33d98cc2
Soroush Yousefpour <soroush.y@affinidi.com>
2025-08-01 11:35:12 +0200

    Revert "feat: jcs variation (#170)"
    This reverts commit 109cb86ff907159cf5f0d298387c614995befd0c.

--- lib/src/credentials/proof/data_integrity_ecdsa_suite.dart
+++ lib/src/credentials/proof/data_integrity_ecdsa_suite.dart
@@ -4,20 +4,15 @@ import 'package:json_ld_processor/json_ld_processor.dart';
 
 import '../../did/did_signer.dart';
 import '../../did/public_key_utils.dart';
-import '../../did/universal_did_resolver.dart';
 import '../../exceptions/ssi_exception.dart';
 import '../../exceptions/ssi_exception_type.dart';
 import '../../types.dart';
 import 'base_data_integrity_verifier.dart';
-import 'base_jcs_generator.dart';
-import 'base_jcs_verifier.dart';
 import 'embedded_proof.dart';
 import 'embedded_proof_suite.dart';
-import 'jcs_utils.dart';
 
 const _dataIntegrityType = 'DataIntegrityProof';
 const _ecdsaCryptosuite = 'ecdsa-rdfc-2019';
-const _ecdsaJcsCryptosuite = 'ecdsa-jcs-2019';
 const _dataIntegrityContext = 'https://w3id.org/security/data-integrity/v2';
 
 /// Generates Data Integrity Proofs using the ecdsa-rdfc-2019 cryptosuite.
@@ -43,12 +38,11 @@ class DataIntegrityEcdsaGenerator extends EmbeddedProofSuiteCreateOptions
     super.domain,
     super.proofValueMultiBase,
   }) {
-    final expectedSchemes = cryptosuiteToScheme[_ecdsaCryptosuite];
-    if (expectedSchemes == null ||
-        !expectedSchemes.contains(signer.signatureScheme)) {
+    final expectedScheme = cryptosuiteToScheme[_ecdsaCryptosuite];
+    if (signer.signatureScheme != expectedScheme) {
       throw SsiException(
         message:
-            'Signer algorithm ${signer.signatureScheme} is not compatible with $_ecdsaCryptosuite. Expected $expectedSchemes.',
+            'Signer algorithm ${signer.signatureScheme} is not compatible with $_ecdsaCryptosuite. Expected $expectedScheme.',
         code: SsiExceptionType.unsupportedSignatureScheme.code,
       );
     }
@@ -109,8 +103,6 @@ class DataIntegrityEcdsaGenerator extends EmbeddedProofSuiteCreateOptions
 ///
 /// Normalizes and hashes the credential and proof separately, then verifies
 /// the combined hash against the provided proof signature using the issuer's DID key.
-@Deprecated(
-    'Use DataIntegrityEcdsaRdfcVerifier for ecdsa-rdfc-2019 cryptosuite instead')
 class DataIntegrityEcdsaVerifier extends BaseDataIntegrityVerifier {
   /// Constructs a new [DataIntegrityEcdsaVerifier].
   ///
@@ -162,197 +154,5 @@ class DataIntegrityEcdsaVerifier extends BaseDataIntegrityVerifier {
       hash,
       _ecdsaCryptosuite,
     );
-  }
-}
-
-/// Verifies Data Integrity Proofs signed with the ecdsa-rdfc-2019 cryptosuite.
-///
-/// Normalizes and hashes the credential and proof separately, then verifies
-/// the combined hash against the provided proof signature using the issuer's DID key.
-class DataIntegrityEcdsaRdfcVerifier extends BaseDataIntegrityVerifier {
-  /// Constructs a new [DataIntegrityEcdsaRdfcVerifier].
-  ///
-  /// [issuerDid]: The expected issuer DID.
-  /// [getNow]: Optional time supplier (defaults to `DateTime.now`).
-  /// [domain]: Optional expected domain(s).
-  /// [challenge]: Optional expected challenge string.
-  DataIntegrityEcdsaRdfcVerifier({
-    required super.issuerDid,
-    super.getNow,
-    super.domain,
-    super.challenge,
-    super.customDocumentLoader,
-  });
-
-  @override
-  String get expectedProofType => _dataIntegrityType;
-
-  @override
-  String get expectedCryptosuite => _ecdsaCryptosuite;
-
-  @override
-  String get contextUrl => _dataIntegrityContext;
-
-  @override
-  String get proofValueField => 'proofValue';
-
-  @override
-  Future<Uint8List> computeSignatureHash(
-    Map<String, dynamic> proof,
-    Map<String, dynamic> unsignedCredential,
-    Future<RemoteDocument?> Function(Uri url, LoadDocumentOptions? options)
-        documentLoader,
-  ) async {
-    return computeDataIntegrityHash(proof, unsignedCredential, documentLoader);
-  }
-
-  @override
-  Future<bool> verifySignature(
-    String proofValue,
-    String issuerDid,
-    Uri verificationMethod,
-    Uint8List hash,
-  ) async {
-    return verifyDataIntegritySignature(
-      proofValue,
-      issuerDid,
-      verificationMethod,
-      hash,
-      _ecdsaCryptosuite,
-    );
-  }
-}
-
-/// Generates Data Integrity Proofs using the ecdsa-jcs-2019 cryptosuite.
-///
-/// Signs Verifiable Credentials by canonicalizing the credential and the proof using JCS,
-/// hashing them, and then signing the combined hash using a [DidSigner].
-class DataIntegrityEcdsaJcsGenerator extends BaseJcsGenerator {
-  /// Constructs a new [DataIntegrityEcdsaJcsGenerator].
-  ///
-  /// [signer]: The DID signer responsible for creating the proof signature.
-  /// Optional parameters like [proofPurpose], [customDocumentLoader], [expires],
-  /// [challenge], [domain], and [proofValueMultiBase] configure the proof metadata.
-  DataIntegrityEcdsaJcsGenerator({
-    required super.signer,
-    super.proofPurpose,
-    super.customDocumentLoader,
-    super.expires,
-    super.challenge,
-    super.domain,
-    super.proofValueMultiBase,
-  });
-
-  @override
-  String get cryptosuite => _ecdsaJcsCryptosuite;
-
-  @override
-  HashingAlgorithm get hashingAlgorithm =>
-      signer.signatureScheme.hashingAlgorithm;
-
-  @override
-  void validateSignerCompatibility(DidSigner signer) {
-    if (signer.signatureScheme != SignatureScheme.ecdsa_p256_sha256 &&
-        signer.signatureScheme != SignatureScheme.ecdsa_p384_sha384) {
-      throw SsiException(
-        message:
-            'Signer algorithm ${signer.signatureScheme} is not compatible with $_ecdsaJcsCryptosuite. Expected P-256 (SHA-256) or P-384 (SHA-384).',
-        code: SsiExceptionType.unsupportedSignatureScheme.code,
-      );
-    }
-  }
-}
-
-/// Verifies Data Integrity Proofs signed with the ecdsa-jcs-2019 cryptosuite.
-///
-/// Canonicalizes using JCS and hashes the credential and proof separately, then verifies
-/// the combined hash against the provided proof signature using the issuer's DID key.
-class DataIntegrityEcdsaJcsVerifier extends BaseJcsVerifier {
-  /// Constructs a new [DataIntegrityEcdsaJcsVerifier].
-  ///
-  /// [verifierDid]: The DID of the issuer whose credential this verifier will validate.
-  /// [getNow]: Optional time supplier (defaults to `DateTime.now`).
-  /// [domain]: Optional expected domain(s).
-  /// [challenge]: Optional expected challenge string.
-  DataIntegrityEcdsaJcsVerifier({
-    required super.verifierDid,
-    super.getNow,
-    super.domain,
-    super.challenge,
-    super.customDocumentLoader,
-  });
-
-  @override
-  String get expectedJcsCryptosuite => _ecdsaJcsCryptosuite;
-
-  @override
-  HashingAlgorithm get hashingAlgorithm {
-    // For ECDSA JCS, we can't determine the hash algorithm statically
-    // since it supports both P-256 (SHA-256) and P-384 (SHA-384).
-    // The actual algorithm is determined dynamically in computeSignatureHash.
-    throw UnsupportedError(
-        'ECDSA JCS requires dynamic hash algorithm determination');
-  }
-
-  @override
-  Future<Uint8List> computeSignatureHash(
-    Map<String, dynamic> proof,
-    Map<String, dynamic> unsignedCredential,
-    Future<RemoteDocument?> Function(Uri url, LoadDocumentOptions? options)
-        documentLoader,
-  ) async {
-    // For ecdsa-jcs-2019, we need to dynamically determine the signature scheme
-    // by examining the verification method since it supports both P-256 and P-384
-    final signatureScheme =
-        await _getSignatureSchemeFromVerificationMethod(proof);
-    return JcsUtils.computeDataIntegrityJcsHash(
-        proof, unsignedCredential, signatureScheme.hashingAlgorithm);
-  }
-
-  @override
-  Future<SignatureScheme> getSignatureScheme(Uri verificationMethod) async {
-    return _getSignatureSchemeFromDid(verificationMethod);
-  }
-
-  /// Determines the signature scheme from the verification method.
-  Future<SignatureScheme> _getSignatureSchemeFromVerificationMethod(
-      Map<String, dynamic> proof) async {
-    final verificationMethodUri = proof['verificationMethod'] as String?;
-    if (verificationMethodUri == null) {
-      throw SsiException(
-        message: 'Missing verificationMethod in proof',
-        code: SsiExceptionType.unableToParseVerifiableCredential.code,
-      );
-    }
-
-    return _getSignatureSchemeFromDid(Uri.parse(verificationMethodUri));
-  }
-
-  /// Determines the signature scheme from the verification method.
-  Future<SignatureScheme> _getSignatureSchemeFromDid(
-      Uri verificationMethod) async {
-    // Resolve the DID to get the verification method
-    final didDocument =
-        await UniversalDIDResolver.defaultResolver.resolveDid(issuerDid);
-
-    // Find the verification method in the DID document
-    final vm = didDocument.verificationMethod
-        .where((vm) =>
-            vm.id == verificationMethod.toString() ||
-            vm.id.endsWith('#${verificationMethod.toString().split('#').last}'))
-        .firstOrNull;
-
-    if (vm == null) {
-      throw SsiException(
-        message:
-            'Verification method $verificationMethod not found in DID document',
-        code: SsiExceptionType.invalidDidDocument.code,
-      );
-    }
-
-    // Get the JWK and determine the signature scheme
-    final jwk = vm.asJwk();
-    final jwkMap = jwk.toJson();
-    return getEcdsaSignatureScheme(jwkMap);
   }
 }
--- test/credentials/proof/data_integrity_ecdsa_test.dart
+++ test/credentials/proof/data_integrity_ecdsa_test.dart
@@ -1,4 +1,5 @@
 import 'package:base_codecs/base_codecs.dart';
+import 'package:ssi/src/did/public_key_utils.dart';
 import 'package:ssi/ssi.dart';
 import 'package:test/test.dart';
 
@@ -10,8 +11,6 @@ void main() async {
   );
 
   final signer = await initP256Signer(seed);
-  final p384Signer = await initP384Signer(seed);
-  final edSigner = await initEdSigner(seed);
 
   group('Test Data Integrity ECDSA VC issuance', () {
     final multiBaseList = [MultiBase.base58bitcoin, MultiBase.base64UrlNoPad];
@@ -56,7 +55,7 @@ void main() async {
           );
 
           final proofVerifier =
-              DataIntegrityEcdsaRdfcVerifier(issuerDid: signer.did);
+              DataIntegrityEcdsaVerifier(issuerDid: signer.did);
 
           final verificationResult =
               await proofVerifier.verify(issuedCredential.toJson());
@@ -126,200 +125,5 @@ void main() async {
         });
       });
     }
-  });
-
-  group('Test Data Integrity ECDSA-JCS VC issuance', () {
-    test('Create and verify Data Integrity ECDSA-JCS proof with P-256',
-        () async {
-      final unsignedCredential = MutableVcDataModelV1(
-        context: [
-          'https://www.w3.org/2018/credentials/v1',
-          'https://schema.affinidi.com/UserProfileV1-0.jsonld'
-        ],
-        id: Uri.parse('uuid:123456abcd'),
-        type: {'VerifiableCredential', 'UserProfile'},
-        credentialSubject: [
-          MutableCredentialSubject({
-            'Fname': 'Fname',
-            'Lname': 'Lame',
-            'Age': '22',
-            'Address': 'Eihhornstr'
-          })
-        ],
-        holder: MutableHolder.uri('did:example:1'),
-        credentialSchema: [
-          MutableCredentialSchema(
-              id: Uri.parse('https://schema.affinidi.com/UserProfileV1-0.json'),
-              type: 'JsonSchemaValidator2018')
-        ],
-        issuanceDate: DateTime.now(),
-        issuer: Issuer.uri(signer.did),
-      );
-
-      final proofGenerator = DataIntegrityEcdsaJcsGenerator(
-        signer: signer,
-      );
-
-      final issuedCredential = await LdVcDm1Suite().issue(
-        unsignedData: VcDataModelV1.fromMutable(unsignedCredential),
-        proofGenerator: proofGenerator,
-      );
-
-      final proofVerifier =
-          DataIntegrityEcdsaJcsVerifier(verifierDid: signer.did);
-
-      final verificationResult =
-          await proofVerifier.verify(issuedCredential.toJson());
-
-      expect(verificationResult.isValid, true);
-      expect(verificationResult.errors, isEmpty);
-      expect(verificationResult.warnings, isEmpty);
-
-      final proof = issuedCredential.toJson()['proof'] as Map<String, dynamic>;
-      expect(proof['type'], 'DataIntegrityProof');
-      expect(proof['cryptosuite'], 'ecdsa-jcs-2019');
-      expect(proof['proofValue'], isNotNull);
-      expect(proof['proofValue'], startsWith('z')); // base58-btc multibase
-    });
-
-    test('Create and verify Data Integrity ECDSA-JCS proof with P-384',
-        () async {
-      final unsignedCredential = MutableVcDataModelV1(
-        context: [
-          'https://www.w3.org/2018/credentials/v1',
-          'https://schema.affinidi.com/UserProfileV1-0.jsonld'
-        ],
-        id: Uri.parse('uuid:123456abcd'),
-        type: {'VerifiableCredential', 'UserProfile'},
-        credentialSubject: [
-          MutableCredentialSubject({
-            'Fname': 'Fname',
-            'Lname': 'Lame',
-            'Age': '22',
-            'Address': 'Eihhornstr'
-          })
-        ],
-        holder: MutableHolder.uri('did:example:1'),
-        credentialSchema: [
-          MutableCredentialSchema(
-              id: Uri.parse('https://schema.affinidi.com/UserProfileV1-0.json'),
-              type: 'JsonSchemaValidator2018')
-        ],
-        issuanceDate: DateTime.now(),
-        issuer: Issuer.uri(p384Signer.did),
-      );
-
-      final proofGenerator = DataIntegrityEcdsaJcsGenerator(
-        signer: p384Signer,
-      );
-
-      final issuedCredential = await LdVcDm1Suite().issue(
-        unsignedData: VcDataModelV1.fromMutable(unsignedCredential),
-        proofGenerator: proofGenerator,
-      );
-
-      final proofVerifier =
-          DataIntegrityEcdsaJcsVerifier(verifierDid: p384Signer.did);
-
-      final verificationResult =
-          await proofVerifier.verify(issuedCredential.toJson());
-
-      expect(verificationResult.isValid, true);
-      expect(verificationResult.errors, isEmpty);
-      expect(verificationResult.warnings, isEmpty);
-
-      final proof = issuedCredential.toJson()['proof'] as Map<String, dynamic>;
-      expect(proof['type'], 'DataIntegrityProof');
-      expect(proof['cryptosuite'], 'ecdsa-jcs-2019');
-      expect(proof['proofValue'], isNotNull);
-      expect(proof['proofValue'], startsWith('z')); // base58-btc multibase
-    });
-
-    test('JCS context validation works correctly', () async {
-      final unsignedCredential = MutableVcDataModelV1(
-        context: [
-          'https://www.w3.org/2018/credentials/v1',
-          'https://schema.affinidi.com/UserProfileV1-0.jsonld'
-        ],
-        id: Uri.parse('uuid:123456abcd'),
-        type: {'VerifiableCredential', 'UserProfile'},
-        credentialSubject: [
-          MutableCredentialSubject({'name': 'Test User'})
-        ],
-        issuanceDate: DateTime.now(),
-        issuer: Issuer.uri(signer.did),
-      );
-
-      final proofGenerator = DataIntegrityEcdsaJcsGenerator(
-        signer: signer,
-      );
-
-      final issuedCredential = await LdVcDm1Suite().issue(
-        unsignedData: VcDataModelV1.fromMutable(unsignedCredential),
-        proofGenerator: proofGenerator,
-      );
-
-      // Verify that the proof contains the same @context as the document
-      final credentialJson = issuedCredential.toJson();
-      final proof = credentialJson['proof'] as Map<String, dynamic>;
-
-      // The proof should NOT have @context in the final form (it's removed after signing)
-      expect(proof.containsKey('@context'), false);
-
-      final proofVerifier =
-          DataIntegrityEcdsaJcsVerifier(verifierDid: signer.did);
-      final verificationResult = await proofVerifier.verify(credentialJson);
-
-      expect(verificationResult.isValid, true);
-      expect(verificationResult.errors, isEmpty);
-    });
-
-    test('ECDSA-JCS rejects unsupported signature schemes', () {
-      expect(
-        () => DataIntegrityEcdsaJcsGenerator(signer: edSigner),
-        throwsA(isA<SsiException>()),
-      );
-    });
-
-    test('Verify Data Integrity ECDSA-JCS proof through LdBaseSuite', () async {
-      final unsignedCredential = MutableVcDataModelV1(
-        context: [
-          'https://www.w3.org/2018/credentials/v1',
-          'https://schema.affinidi.com/UserProfileV1-0.jsonld'
-        ],
-        id: Uri.parse('uuid:123456abcd'),
-        type: {'VerifiableCredential', 'UserProfile'},
-        credentialSubject: [
-          MutableCredentialSubject({
-            'Fname': 'Fname',
-            'Lname': 'Lame',
-            'Age': '22',
-            'Address': 'Eihhornstr'
-          })
-        ],
-        holder: MutableHolder.uri('did:example:1'),
-        credentialSchema: [
-          MutableCredentialSchema(
-              id: Uri.parse('https://schema.affinidi.com/UserProfileV1-0.json'),
-              type: 'JsonSchemaValidator2018')
-        ],
-        issuanceDate: DateTime.now(),
-        issuer: Issuer.uri(signer.did),
-      );
-
-      final proofGenerator = DataIntegrityEcdsaJcsGenerator(
-        signer: signer,
-      );
-
-      final issuedCredential = await LdVcDm1Suite().issue(
-        unsignedData: VcDataModelV1.fromMutable(unsignedCredential),
-        proofGenerator: proofGenerator,
-      );
-
-      final validationResult =
-          await LdVcDm1Suite().verifyIntegrity(issuedCredential);
-
-      expect(validationResult, true);
-    });
   });
 }
