05eb76119342cc462e939fd86d3e419314336c47
Mohamed Abdelsamei <mohamed.e@affinidi.com>
2025-08-06 11:52:44 +0200

    feat: enhance UniversalPresentationVerifier to support credential verification

--- lib/src/credentials/presentations/suites/universal_presentation_verifier.dart
+++ lib/src/credentials/presentations/suites/universal_presentation_verifier.dart
@@ -1,5 +1,7 @@
 import '../../../types.dart';
 import '../../proof/embedded_proof_suite.dart';
+import '../../suites/universal_verifier.dart';
+import '../../verification/vc_verifier.dart';
 import '../models/parsed_vp.dart';
 import '../verification/delegation_vc_verifier.dart';
 import '../verification/vp_expiry_verifier.dart';
@@ -23,6 +25,9 @@ final class UniversalPresentationVerifier {
   /// The list of verifiers
   final List<VpVerifier> customVerifiers;
 
+  /// The list of vc verifiers
+  final List<VcVerifier> credentialCustomVerifiers;
+
   /// Custom document loader for loading external resources during verification.
   final DocumentLoader? customDocumentLoader;
 
@@ -40,26 +45,38 @@ final class UniversalPresentationVerifier {
   /// Optionally accepts [customVerifiers] to extend validation logic.
   UniversalPresentationVerifier({
     List<VpVerifier>? customVerifiers,
+    List<VcVerifier>? credentialCustomVerifiers,
     this.customDocumentLoader,
-  }) : customVerifiers = customVerifiers ?? [];
+  })  : customVerifiers = customVerifiers ?? [],
+        credentialCustomVerifiers = credentialCustomVerifiers ?? [];
 
   /// Verifies the given [ParsedVerifiablePresentation] using all registered verifiers.
   ///
   /// Returns a [VerificationResult] that contains any collected errors and warnings.
-  Future<VerificationResult> verify(ParsedVerifiablePresentation data) async {
+  Future<VerificationResult> verify(ParsedVerifiablePresentation vp) async {
     final errors = <String>[];
     final warnings = <String>[];
 
     for (final verifier in defaultVerifiers) {
-      final result = await verifier.verify(data);
+      final result = await verifier.verify(vp);
       errors.addAll(result.errors);
       warnings.addAll(result.warnings);
     }
 
     for (final customVerifier in customVerifiers) {
-      var verifResult = await customVerifier.verify(data);
+      var verifResult = await customVerifier.verify(vp);
       errors.addAll(verifResult.errors);
       warnings.addAll(verifResult.warnings);
+    }
+
+    final vcVerifier = UniversalVerifier(
+        customVerifiers: credentialCustomVerifiers,
+        customDocumentLoader: customDocumentLoader);
+
+    for (final credential in vp.verifiableCredential) {
+      final vcVerificationResult = await vcVerifier.verify(credential);
+      errors.add(vcVerificationResult.errors);
+      warnings.add(vcVerificationResult.warnings);
     }
 
     return VerificationResult.fromFindings(
--- lib/src/credentials/presentations/verification/vp_integrity_verifier.dart
+++ lib/src/credentials/presentations/verification/vp_integrity_verifier.dart
@@ -1,7 +1,6 @@
 import '../../../../ssi.dart';
 
-/// Verifier that ensures the integrity of both the Verifiable Presentation (VP)
-/// and its embedded Verifiable Credentials (VCs).
+/// Verifier that ensures the integrity of the Verifiable Presentation (VP).
 ///
 /// This verifier fails fast: it stops at the first encountered integrity issue./// Example:
 /// ```dart
@@ -37,17 +36,6 @@ class VpIntegrityVerifier implements VpVerifier {
           errors: [SsiExceptionType.failedIntegrityVerification.code],
         ),
       );
-    }
-    // Create instance of [VcIntegrityVerifier] for credential-level integrity checks.
-    final vcIntegrityVerifier =
-        VcIntegrityVerifier(customDocumentLoader: customDocumentLoader);
-
-    for (final credential in data.verifiableCredential) {
-      var vcIntegrity = await vcIntegrityVerifier.verify(credential);
-
-      if (!vcIntegrity.isValid) {
-        return vcIntegrity;
-      }
     }
 
     return Future.value(
