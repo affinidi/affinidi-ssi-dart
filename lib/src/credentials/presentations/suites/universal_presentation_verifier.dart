abed6f136c913cc37f1a67e62c90f7123195b304
Mohamed Abdelsamei <mohamed.e@affinidi.com>
2025-08-07 11:56:46 +0200

    feat: enhance UniversalPresentationVerifier to allow custom credential verifiers (#176)
    UniversalPresentationVerifier:
    - Updated UniversalPresentationVerifier to use UniversalVerifier for individual credential verification within presentations.
    - Added support for custom VC verifiers (customVclVerifiers) to allow more flexible credential-level verification.
    
    VpIntegrityVerifier:
    - Updated to handle VP-level integrity checks only. Individual credentials within the VP should be verified using VcIntegrityVerifier.

--- lib/src/credentials/presentations/suites/universal_presentation_verifier.dart
+++ lib/src/credentials/presentations/suites/universal_presentation_verifier.dart
@@ -1,5 +1,7 @@
 import '../../../types.dart';
 import '../../proof/embedded_proof_suite.dart';
+import '../../suites/universal_verifier.dart';
+import '../../verification/vc_verifier.dart';
 import '../models/parsed_vp.dart';
 import '../verification/delegation_vc_verifier.dart';
 import '../verification/vp_expiry_verifier.dart';
@@ -23,6 +25,9 @@ final class UniversalPresentationVerifier {
   /// The list of verifiers
   final List<VpVerifier> customVerifiers;
 
+  /// The list of vc verifiers.
+  final List<VcVerifier> customVclVerifiers;
+
   /// Custom document loader for loading external resources during verification.
   final DocumentLoader? customDocumentLoader;
 
@@ -40,26 +45,39 @@ final class UniversalPresentationVerifier {
   /// Optionally accepts [customVerifiers] to extend validation logic.
   UniversalPresentationVerifier({
     List<VpVerifier>? customVerifiers,
+    List<VcVerifier>? customVclVerifiers,
     this.customDocumentLoader,
-  }) : customVerifiers = customVerifiers ?? [];
+  })  : customVerifiers = customVerifiers ?? [],
+        customVclVerifiers = customVclVerifiers ?? [];
 
   /// Verifies the given [ParsedVerifiablePresentation] using all registered verifiers.
   ///
   /// Returns a [VerificationResult] that contains any collected errors and warnings.
-  Future<VerificationResult> verify(ParsedVerifiablePresentation data) async {
+  Future<VerificationResult> verify(ParsedVerifiablePresentation vp) async {
     final errors = <String>[];
     final warnings = <String>[];
 
     for (final verifier in defaultVerifiers) {
-      final result = await verifier.verify(data);
+      final result = await verifier.verify(vp);
       errors.addAll(result.errors);
       warnings.addAll(result.warnings);
     }
 
     for (final customVerifier in customVerifiers) {
-      var verifResult = await customVerifier.verify(data);
-      errors.addAll(verifResult.errors);
-      warnings.addAll(verifResult.warnings);
+      var verificationResult = await customVerifier.verify(vp);
+      errors.addAll(verificationResult.errors);
+      warnings.addAll(verificationResult.warnings);
+    }
+
+    final vcVerifier = UniversalVerifier(
+        customDocumentLoader: customDocumentLoader,
+        customVerifiers: customVclVerifiers);
+
+    // Verify each credential using the UniversalVerifier
+    for (final credential in vp.verifiableCredential) {
+      final vcVerificationResult = await vcVerifier.verify(credential);
+      errors.addAll(vcVerificationResult.errors);
+      warnings.addAll(vcVerificationResult.warnings);
     }
 
     return VerificationResult.fromFindings(
