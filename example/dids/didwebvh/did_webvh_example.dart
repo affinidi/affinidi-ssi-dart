7428098feb117119cb210630b90b783b5df7e920
Mohamed Abdelsamei <mohamed.e@affinidi.com>
2026-02-20 11:18:58 +0100

    fix: update example DID in did_webvh_example.dart

--- example/dids/didwebvh/did_webvh_example.dart
+++ example/dids/didwebvh/did_webvh_example.dart
@@ -2,7 +2,7 @@ import 'package:ssi/src/did/did_webvh.dart';
 
 Future<void> main() async {
   final did =
-      'did:webvh:QmXi6hYqAUBKevKkUAbETFa6LKjvCSnuxdS1icQtiR4SAi:raw.githubusercontent.com:affinidi:affinidi-ssi-dart:refs:heads:main:example:dids:didwebvh:alice-with-witness';
+      'did:webvh:Qme2PYT44CZFzmT4ReHqGXAX4SVijyLCDJVELa6iBqXV1M:raw.githubusercontent.com:affinidi:affinidi-ssi-dart:refs:heads:main:example:dids:didwebvh:alice-with-witness';
   final parsedDid = DidWebVh.parse(did);
   print('Parsed DID: $parsedDid');
   final doc = await parsedDid.resolveDid();
